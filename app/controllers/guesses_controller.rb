c0235f56ade76c5e1be98133d80bb4c361869797
normoneil <normoneil@users.noreply.github.com>
2016-10-30 19:28:18 -0500

    93%, 1 failure.  Not sure what it is yet...

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -1,10 +1,23 @@
 class GuessesController < ApplicationController
   def index
-    # g = Guess.new
-    # g.first_number = params["first_num"]
-    # g.save
-    #
-    # @guesses = params["first_number", "second_number"]
+    # 1.is_a? Integer
+    @first_number = params["first_number"]
+    @second_number = params["second_number"]
+    @third_number = params["third_number"]
+    g = Guess.new
+    g.first_number = @first_number
+    g.second_number = @second_number
+    g.third_number = @third_number
+    if g.first_number != nil && g.second_number != nil && g.third_number != nil
+      if @first_number < @second_number && @second_number < @third_number
+        @outcome = "Yes!"
+      else
+        @outcome = "No."
+      end
+      g.result = @outcome
+      g.save
+    end
+    @list = Guess.all
     render("guesses/index.html.erb")
   end
 
@@ -12,5 +25,10 @@ class GuessesController < ApplicationController
     # params = {"rule"=>"Increasing numbers"}
     @user_answer = params["rule"]
     render("guesses/answer.html.erb")
+  end
+
+  def clear
+    Guess.delete_all
+    render("guesses/index.html.erb")
   end
 end
